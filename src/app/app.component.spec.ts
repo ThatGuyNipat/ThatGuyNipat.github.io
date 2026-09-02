440a61dee2c1cbaec5cedcb3c8692ea31f86f519
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2026-09-02 21:17:26 +0700

    issue-add-personal-profile

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-cv'`, () => {
+  it(`should have as title 'Nipat P.'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-cv');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('web-cv app is running!');
+    expect(app.title).toEqual('Nipat P.');
   });
 });
