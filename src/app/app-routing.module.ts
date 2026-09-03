3c2e0beb1cb7a91430672147267f3a4725cc2563
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2026-09-04 00:20:38 +0700

    Theme change - Game menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { EducationComponent } from './pages/education/education.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent },
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   // { path: '**', component: ErrorComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { EducationComponent } from './pages/education/education.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    LandingPageComponent,
+    EducationComponent,
+    InventoryComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
